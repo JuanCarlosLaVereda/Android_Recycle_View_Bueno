23d36248103322923e92891c8b74952b2a5dac0d
jsantamariap <juasanper2@alu.edu.gva.es>
2024-04-16 20:25:26 +0200

    v.0

--- app/src/main/java/com/example/android_recycleview_bueno/model/ProfesionRepository.java
+++ app/src/main/java/com/example/android_recycleview_bueno/model/ProfesionRepository.java
@@ -1,4 +1,19 @@
 package com.example.android_recycleview_bueno.model;
 
+import java.util.List;
+
 public class ProfesionRepository {
+    private List<Profesion> profesiones;
+    private  static ProfesionRepository instace;
+
+    private ProfesionRepository(){
+
+    }
+
+    public static ProfesionRepository getInstance(){
+        if (instace==null){
+            instace = new ProfesionRepository();
+        }
+        return instace;
+    }
 }
